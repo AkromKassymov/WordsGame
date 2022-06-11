d2e9040ddc3690ecf7e67bc91d9801dfaa22a522
Akrom Kassymov <akrom222@gmail.com>
2022-06-11 07:22:55 +0600

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Akrom on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
